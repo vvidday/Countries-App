20f48f4f810a08376b23c4efa80fbcbb016abf9c
vvidday <davids89034@gmail.com>
2022-07-18 20:51:34 +0800

    skeleton navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<script></script>
+
+<template>
+    <div class="bg-lmelements dark:bg-dmelements flex justify-between">
+        <p>Where in the world?</p>
+        <button>Dark Mode</button>
+    </div>
+</template>
